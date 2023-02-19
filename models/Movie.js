3e9051af04cff698017f58a9c547c560e2313628
Cyril Lazar <klazar@mail.ru>
2023-02-19 19:16:25 +1000

    feat: сделал модели

---
+++ models/Movie.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-useless-escape */
+const { Schema, model } = require('mongoose');
+
+const movieSchema = Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validatу: {
+      validator(v) {
+        const regex = /^https?:\/\/([\w\-]+\.)+[a-z]{2,}(\/[\w#\-\.~:\[\]@!\$&'\(\)\*\+,;=,]*)*$/i;
+        return regex.test(v);
+      },
+      message: 'url не прошел валидацию',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        const regex = /^https?:\/\/([\w\-]+\.)+[a-z]{2,}(\/[\w#\-\.~:\[\]@!\$&'\(\)\*\+,;=,]*)*$/i;
+        return regex.test(v);
+      },
+      message: 'url не прошел валидацию',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        const regex = /^https?:\/\/([\w\-]+\.)+[a-z]{2,}(\/[\w#\-\.~:\[\]@!\$&'\(\)\*\+,;=,]*)*$/i;
+        return regex.test(v);
+      },
+      message: 'url не прошел валидацию',
+    },
+  },
+  owner: { type: Schema.Types.ObjectId, ref: 'user', required: true },
+  movieId: { type: String, required: true }, // откуда получают это поле id???
+  nameRU: { type: String, required: true }, // валидация русских букв
+  nameEN: { type: String, required: true }, // валидация англ букв
+});
+
+module.exports = model('movie', movieSchema);
