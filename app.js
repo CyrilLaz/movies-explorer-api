508b15d26ebf2f2fba46eb17f2054b8ffe4fddd8
Cyril Lazar <klazar@mail.ru>
2023-02-19 23:42:09 +1000

    feat: сделал регистрацию авторизацию и выход

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const usersRouter = require('./routers/users');
+const moviesRouter = require('./routers/movies');
+const { createUser, login, logout } = require('./controllers/users');
+
+const { PORT = 3000, PATH_MONGO = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
+
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect(PATH_MONGO);
+
+app.post('/signup', createUser);
+app.post('/signin', login);
+app.post('/signout', logout);
+
+app.use('/users', usersRouter);
+app.use('/movies', moviesRouter);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
--- models/User.js
+++ models/User.js
@@ -1,30 +1,57 @@
 const { Schema, model } = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcryptjs');
+const UncorrectLoginError = require('../errors/UncorrectLoginError');
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator(v) {
-        return isEmail(v);
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator(v) {
+          return isEmail(v);
+        },
+        message: 'Передан не правильный адрес почты',
       },
-      message: 'Передан не правильный адрес почты',
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      select: false,
+    },
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 30,
     },
   },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-    select: false,
-  },
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-});
+  { versionKey: false },
+);
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  // попытаемся найти пользователя по почте
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(
+          new UncorrectLoginError('Неправильные почта или пароль'),
+        );
+      }
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(
+            new UncorrectLoginError('Неправильные почта или пароль'),
+          );
+        }
+        return user.toObject();
+      });
+    });
+};
 
 module.exports = model('user', userSchema);
