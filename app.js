b061a8760ee5e44ddac337e95e1839f81b0c5c85
Cyril Lazar <klazar@mail.ru>
2023-02-20 20:34:21 +1000

    feat:добавил корсы

--- app.js
+++ app.js
@@ -16,12 +16,14 @@ const {
 } = require('./middlewares/validate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const apiRequestLimiter = require('./middlewares/apiRequestLimiter');
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000, PATH_MONGO = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 
 app.use(apiRequestLimiter);
+app.use(cors);
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
